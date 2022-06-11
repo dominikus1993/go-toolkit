a04a33d6d429954c1c352fc6a24941b78c7765fd
Dominik.Kotecki <dominikus1910@gmail.com>
2022-06-11 15:43:16 +0000

    add fromslice and fanin stream

--- channels.go
+++ channels.go
@@ -1,9 +1,49 @@
 package gotoolkit
 
+import (
+	"context"
+	"sync"
+)
+
+func FromSlice[T any](s []T) <-chan T {
+	res := make(chan T)
+	go func() {
+		for _, v := range s {
+			res <- v
+		}
+		close(res)
+	}()
+	return res
+}
+
 func ToSlice[T any](s <-chan T) []T {
 	res := make([]T, 0)
 	for v := range s {
 		res = append(res, v)
 	}
 	return res
+}
+
+func FanIn[T any](ctx context.Context, stream ...<-chan T) <-chan T {
+	var wg sync.WaitGroup
+	out := make(chan T)
+	output := func(c <-chan T, wait *sync.WaitGroup) {
+		defer wait.Done()
+		for v := range c {
+			select {
+			case <-ctx.Done():
+				return
+			case out <- v:
+			}
+		}
+	}
+	wg.Add(len(stream))
+	for _, c := range stream {
+		go output(c, &wg)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
--- channels_test.go
+++ channels_test.go
@@ -1,6 +1,7 @@
 package gotoolkit
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -40,5 +41,26 @@ func BenchmarkToSlice(b *testing.B) {
 		}()
 
 		ToSlice(numbers)
+	}
+}
+
+func TestFanIn(t *testing.T) {
+	numbers := FromSlice(rangeInt(1, 10))
+	numbers2 := FromSlice(rangeInt(10, 20))
+
+	result := FanIn(context.TODO(), numbers, numbers2)
+	subject := ToSlice(result)
+	assert.Len(t, subject, 19)
+	assert.ElementsMatch(t, rangeInt(1, 20), subject)
+}
+
+func BenchmarkFanIn(b *testing.B) {
+	ctx := context.TODO()
+	for n := 0; n < b.N; n++ {
+		numbers := FromSlice(rangeInt(1, 10))
+		numbers2 := FromSlice(rangeInt(10, 20))
+
+		result := FanIn(ctx, numbers, numbers2)
+		ToSlice(result)
 	}
 }
